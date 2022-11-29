e11b16a4a1b0c8b517c41fbadf668804f95167cd
eyeaune <118779927+eyeaune@users.noreply.github.com>
2022-11-28 16:08:00 -0800

    Update Intake.java

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,1 +1,32 @@
+package frc.robot.subsystems;
+
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class IntakeSubSystem extends SubsystemBase {
+
+    private final Solenoid pivot =
+        new Solenoid(Ports.PCM_1, PneumaticsModuleType.REVPH, Ports.PIVOT_CLIMB_SOLENOID);
+
+    public IntakeSubSystem(){}
+   
+
+    @Override
+    public void periodic() {}
+  
+    public boolean getPivotPos() {
+        return pivotPos;
+    }
+}
 
